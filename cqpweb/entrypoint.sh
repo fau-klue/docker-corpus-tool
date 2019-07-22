d67936440d5831ff525c88c106709f617ba708ce
Markus Opolka <markus@martialblog.de>
2019-07-22 14:53:20 +0200

    [wip] add wait for db script

--- cqpweb/entrypoint.sh
+++ cqpweb/entrypoint.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Entrypoint for Docker Container
+set -e
 
-# TODO: Check and Init Database
+# Wait for database connection
+./wait-for-it.sh --timeout=0 ${DB_HOST}:${DB_PORT}
+
+# Check if database is initialized
+
+# Initialize Database
+
 # CREATE DATABASE cqpweb DEFAULT charset utf8;
-# CREATE user cqpweb identified BY 'secret';
+# CREATE user cqpweb identified BY '$DB_PASSWORD';
 # GRANT ALL ON cqpweb.* TO cqpweb;
 # GRANT file ON *.* TO cqpweb;
 
